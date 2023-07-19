466bceb8297c326f999aa8c919a1badc796c55ed
dproks <81188654+dproks@users.noreply.github.com>
2023-07-19 15:03:31 +0300

    settings

---
+++ aiassist-client/src/components/settings.component.tsx
@@ -0,0 +1,12 @@
+import { FButton } from "@/ui/common"
+import SettingsIcon from "@mui/icons-material/Settings"
+
+function Settings() {
+  return (
+    <>
+      <FButton>
+        <SettingsIcon />
+      </FButton>
+    </>
+  )
+}
